b62fda7cbf63bc36190a29d6666f52eace864b90
eleonsolar <eleonsolar@gmail.com>
2016-09-16 22:31:21 -0400

    Update

---
+++ web/backend/main.php
@@ -0,0 +1,37 @@
+<?php
+
+    require '../components/Utilities.php';
+
+    $data = (object)array();
+
+    $name = trim($_POST['name']);
+    $email = trim($_POST['email']);
+    $message = trim($_POST['message']);
+
+    $utilities = new Utilities();
+
+    if(!empty($name)){
+
+      if(!empty($email)){
+
+        if(!empty($message)){
+
+          if($utilities->validateMail($email)){
+
+            $data->name = $name;
+            $data->email = $email;
+            $data->message = $message;
+
+            echo $utilities->sendMail($data);
+          }else{
+            echo "Debe ingresar una dirección de correo válida";
+          }
+        }else {
+          echo "Todos los campos son obligatorios";
+        }
+      }else {
+        echo "Todos los campos son obligatorios";
+      }
+    }else {
+      echo "Todos los campos son obligatorios";
+    }
