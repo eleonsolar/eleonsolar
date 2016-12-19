283b9731599adac96d1039e4c708d38aa28392f5
eleonsolar <eleonsolar@gmail.com>
2016-12-19 00:52:34 -0400

    Update send mail

--- web/components/Utilities.php
+++ web/components/Utilities.php
@@ -1,9 +1,5 @@
 <?php
 
-  $docroot = getenv("DOCUMENT_ROOT");
-
-  require $docroot.'/eleonsolar/web/libs/PHPMailer/PHPMailerAutoload.php';
-
   class Utilities{
 
     public function validate_mail($email)
@@ -20,28 +16,18 @@
 
     public function send_mail($data)
     {
-      $mail = new PHPMailer;
-
-      $mail->isSMTP();
-      $mail->SMTPAuth = true;
-      $mail->SMTPSecure = 'ssl';
-      $mail->Host = 'hostname';
-      $mail->Port = 'port';
-
-      // Enable SMTP authentication
-      $mail->Username = 'username';           // SMTP username
-      $mail->Password = 'password';           // SMTP password
-
-      $mail->CharSet="UTF-8";
-      $mail->setFrom($data->email, $data->name);
-      $mail->addAddress('eleonsolar@gmail.com', 'Edward Esteban León Solar');     // Add a recipient
-
-      $mail->Subject = $data->name.' te quiere contactar';
-      $mail->MsgHTML($data->message.' #Mail:'.$data->email);
-
-      if(!$mail->send())  $result = 'Mailer Error: ' . $mail->ErrorInfo; else $result = 1;
-
-      return $result;
+      //Destination address
+      $to = 'eleonsolar@gmail.com';
+      //Mail content
+      $content = $data->message;
+      //Subject
+      $subject =  $data->name.' te quiere contactar';
+      //Headers
+      $headers = "MIME-Version: 1.0\r\n";
+      $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+      $headers .= "From: Web site eleonsolar \r\n";
+      //Send mail
+      return (mail($to, $subject, $content, $headers))? 1:0;
     }
 
   }
