93691f37a446f0a2dbcf7dabab09aeb4f18b738a
eleonsolar <eleonsolar@gmail.com>
2016-12-19 01:33:29 -0400

    Update mail subject

--- web/components/Utilities.php
+++ web/components/Utilities.php
@@ -16,18 +16,18 @@
 
     public function send_mail($data)
     {
-      //Destination address
-      $to = 'eleonsolar@gmail.com';
-      //Mail content
-      $content = $data->message;
-      //Subject
-      $subject =  $data->name.' te quiere contactar';
-      //Headers
-      $headers = "MIME-Version: 1.0\r\n";
-      $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
-      $headers .= "From: Web site eleonsolar \r\n";
-      //Send mail
-      return (mail($to, $subject, $content, $headers))? 1:0;
+       //Destination address
+       $to = 'eleonsolar@gmail.com';
+       //Mail content
+       $content = $data->message;
+       //Subject
+       $subject =  $data->name.' te quiere contactar';
+       //Headers
+       $headers = "MIME-Version: 1.0\r\n";
+       $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+       $headers .= "From:Web site official  <webmaster@eleonsolar.com> \r\n";
+       //Send mail
+       return (mail($to, $subject, $content, $headers))? 1:0;
     }
 
   }
