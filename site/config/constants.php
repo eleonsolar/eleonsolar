7bd0cc3cf02e10f0489db0ce49290a8602642cb4
eleonsolar <eleonsolar@gmail.com>
2018-02-02 10:40:50 -0400

    delete github url

--- site/config/constants.php
+++ site/config/constants.php
@@ -1,7 +1,5 @@
 <?php
 //LOCALHOST
 //define('WEB_URL', 'http://localhost/eleonsolar/');
-//GITHUB
-//define('WEB_URL', 'https://eleonsolar.github.io/');
 //DOMAIN
 define('WEB_URL', 'http://eleonsolar.com/');
