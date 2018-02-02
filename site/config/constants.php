56a40a72300dfd5efd0a39c395d9e168bed668c5
eleonsolar <eleonsolar@gmail.com>
2018-02-02 10:34:23 -0400

    Reinstall site

---
+++ site/config/constants.php
@@ -0,0 +1,7 @@
+<?php
+//LOCALHOST
+//define('WEB_URL', 'http://localhost/eleonsolar/');
+//GITHUB
+//define('WEB_URL', 'https://eleonsolar.github.io/');
+//DOMAIN
+define('WEB_URL', 'http://eleonsolar.com/');
