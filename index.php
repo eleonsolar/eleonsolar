f1806a14bc18928df4a378ad522e9483def5bb10
Edward Leon <eleonsolar@gmail.com>
2019-06-25 23:54:17 -0400

    Delete comment

--- index.php
+++ index.php
@@ -119,7 +119,7 @@
 		}
 		.btn-back-to-top {
 			border-color: #D56565;
-		}
+		}		
 		</style>
 	</head>
 	<body>
@@ -137,22 +137,6 @@
 
 					<!-- Nav -->
 						<nav id="nav">
-							<!--
-
-								Prologue's nav expects links in one of two formats:
-
-								1. Hash link (scrolls to a different section within the page)
-
-						   <li><a href="#foobar" id="foobar-link" class="icon fa-whatever-icon-you-want skel-layers-ignoreHref"><span class="label">Foobar</span></a></li>
-
-								2. Standard link (sends the user to another page/site)
-
-								   <li><a href="http://foobar.tld" id="foobar-link" class="icon fa-whatever-icon-you-want"><span class="label">Foobar</span></a></li>
-
-							-->
-							<!--<div id="exit-link">
-								<a href="<?php echo WEB_URL; ?>"  class="skel-layers-ignoreHref" title="Exit"><span class="icon fa-sign-out exit"></span></a>
-							</div>-->
 							<ul>
 								<li><a href="#top" id="top-link" class="skel-layers-ignoreHref"><span class="icon fa-home"><?php echo $utilities->lang('title.home.header'); ?></span></a></li>
 								<li><a href="#about" id="about-link" class="skel-layers-ignoreHref"><span class="icon fa-user"><?php echo $utilities->lang('title.aboutme.header'); ?></span></a></li>
